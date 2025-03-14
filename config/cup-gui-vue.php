deb3aef59dd4d98b37fd63f6a4efade443e17427
vagrant <vagrant@homestead>
2025-03-14 09:44:05 -0500

    aggiornata configurazione

--- config/cup-gui-vue.php
+++ config/cup-gui-vue.php
@@ -2,7 +2,8 @@
 
 return [
     'services' => [
-        Marley71\CupSocketServer\WebSockets\SystemService::class //classi che espletano servizi websocket
+        Marley71\CupSocketServer\WebSockets\SystemService::class, //classi che espletano servizi websocket
+        Marley71\CupSocketServer\WebSockets\DbService::class
     ],
     'cupparis-primevue-git' => 'https://github.com/marley71/cupparis-primevue.git', //'git@github.com:marley71/cupparis-primevue.git',
     'cupparis-primevue-branch' => 'features/w-leafleft',  // branch libreria cupparis-primevue
@@ -15,8 +16,8 @@ return [
             "VITE_APP_DINAMIC_CONF"=>"/data/dynamic-conf.json",
             "VITE_APP_DEV_MENU"=>1,
             "VITE_API_MENU"=>"/api/app-menu",
-            "VITE_WEBSOCKET_SERVER"=>"ws://" .  env('VUEAPP_DOMAIN','localhost') . ':' . env('VUEAPP_WEBSOCKET_PORT'),
-            "VITE_WEB_SOCKET_SERVICES"=>"system",   // servizi da utilizzare un sottoinsieme dei services definiti sopra
+            "VITE_WEBSOCKET_SERVER"=>"ws://" .  env('VUEAPP_DOMAIN','localhost') . ':' . env('VUEAPP_WEBSOCKET_PORT',7071),
+            "VITE_WEB_SOCKET_SERVICES"=>"system,db",   // servizi da utilizzare un sottoinsieme dei services definiti sopra
             "VITE_MODE"=> "dev",
             "VITE_RESOURCES_PATH" => base_path(env('VUEAPP_APPLICATION_PATH','resources/vue-application')), // path dove ci saranno le risorse dell'applicazione
             "APP_DOMAIN" => env('VUEAPP_DOMAIN'),
@@ -24,8 +25,8 @@ return [
             "APP_HOST"=> env('VUEAPP_HOST','0.0.0.0'), // url in ascolto del server npm vite
             "APP_PORT"=> env('VUEAPP_PORT','8001'),  // porta in ascolto del server npm vite
             "APP_RESOURCES_PATH" => base_path(env('VUEAPP_APPLICATION_PATH','resources/vue-application')), // path dove ci saranno le risorse dell'applicazione
-            "APP_CUPPARIS_PRIMEVUE" => base_path(env('VUEAPP_FOLDER','resources'))  . '/cupparis-primevue' // path dove si trova la libreria cupparis-primevue
-
+            "APP_CUPPARIS_PRIMEVUE" => base_path(env('VUEAPP_FOLDER','resources'))  . '/cupparis-primevue', // path dove si trova la libreria cupparis-primevue
+            "APP_CERT_PATH" => env('VUEAPP_CERT_FOLDER',null)
         ],
         'production' => [ // crea il file env per vite .env.production
             "VITE_APP_USE_API"=>1,
@@ -40,6 +41,8 @@ return [
     "application_path" => base_path(env('VUEAPP_FOLDER','resources') . '/vue-application'),
     "roma_path" => base_path(env('VUEAPP_FOLDER','resources') . '/roma-vue-4.0.0'),
     "cupparis_primevue_path" => base_path(env('VUEAPP_FOLDER','resources') . '/cupparis-primevue'),
+    "php_bin" => env('PHP_BIN','php'),
+    // menu applicazione se visibile non e' settato la voce viene vista da tutti
     "menu" =>  [
         [
             "label" =>'Dashboard', "icon"=>'fa fa-tachometer-alt', "to"=>'/',
