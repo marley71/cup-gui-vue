3c630ab349facb97e43450c399de7a8e8a30d077
Giacomo Terreni <giacomo.terreni@gmail.com>
2025-11-28 11:08:52 +0100

    cupparis entity migliorato

--- resources/vue-application-v4/src/application/ModelConfs/ModelCupparisEntity.js
+++ resources/vue-application-v4/src/application/ModelConfs/ModelCupparisEntity.js
@@ -1,6 +1,7 @@
 import cs from 'cupparis-primevue';
 import CrudCore from "@cupparis-lib/lib/CrudCore";
 import Server from "@cupparis-lib/lib/Server";
+
 export default {
     modelName: 'cupparis_entity',
     search: {
@@ -31,19 +32,19 @@ export default {
             'action-rollback',
         ],
         actionsConfig: {
-            'action-migrate' : {
-                actionType : 'record',
-                title : 'Migrate',
+            'action-migrate': {
+                actionType: 'record',
+                title: 'Migrate',
                 buttonClass: 'p-button-outlined p-button-success',
-                icon : 'fa fa-wrench',
-                text : 'Migrate',
-                json : null,
-                autoreloadView : true,
-                execute (event) {
+                icon: 'fa fa-wrench',
+                text: 'Migrate',
+                json: null,
+                autoreloadView: true,
+                execute(event) {
                     let tA = this;
-                    return new Promise(function (resolve,reject) {
+                    return new Promise(function (resolve, reject) {
                         tA._migrate(function (esito) {
-                            console.log('migrate Event',event,esito);
+                            console.log('migrate Event', event, esito);
                             if (esito) {
                                 resolve();
                             } else {
@@ -55,16 +56,21 @@ export default {
 
                 },
 
-                _migrate (callback) {
+                _migrate(callback) {
                     var that = this;
                     var r = that.createRoute('foormaction_record');
                     r.setValues({
-                        modelName: that.view.modelName,
+                        modelName: that.viewInstance.modelName,
                         actionName: 'migrate',
                     });
+
                     r.setParams({
                         id: that.modelData.id,
+                        //cosa: that.modelData.cosa_migrare,
+                        cosa: that.viewInstance.getValue()[that.index].cosa_migrare,
                     });
+
+                    // console.log("ROUTE MIG",r,that.modelData.cosa_migrare,that.viewInstance.getValue()[that.index].cosa_migrare)//that.viewInstance.getRowDataByKey(that.index))
                     that.waitStart()
                     Server.route(r, function (json) {
                         that.waitEnd();
@@ -74,25 +80,25 @@ export default {
                             return;
                         }
                         CrudCore.alertSuccess("Migrazione eseguita con successo")
-                        that.view.reload();
+                        that.viewInstance.reload();
                     })
                 }
 
             },
 
-            'action-rollback' : {
-                actionType : 'record',
-                title : 'Rollback',
+            'action-rollback': {
+                actionType: 'record',
+                title: 'Rollback',
                 buttonClass: 'p-button-outlined p-button-danger',
-                icon : 'fa fa-arrow-down',
-                text : 'Rollback',
-                json : null,
-                autoreloadView : true,
-                execute (event) {
+                icon: 'fa fa-arrow-down',
+                text: 'Rollback',
+                json: null,
+                autoreloadView: true,
+                execute(event) {
                     let tA = this;
-                    return new Promise(function (resolve,reject) {
+                    return new Promise(function (resolve, reject) {
                         tA._rollback(function (esito) {
-                            console.log('rollback Event',event,esito);
+                            console.log('rollback Event', event, esito);
                             if (esito) {
                                 resolve();
                             } else {
@@ -104,15 +110,17 @@ export default {
 
                 },
 
-                _rollback (callback) {
+                _rollback(callback) {
                     var that = this;
                     var r = that.createRoute('foormaction_record');
                     r.setValues({
-                        modelName: that.view.modelName,
+                        modelName: that.viewInstance.modelName,
                         actionName: 'rollback',
                     });
                     r.setParams({
                         id: that.modelData.id,
+                        //cosa: that.modelData.cosa_migrare,
+                        cosa: that.viewInstance.getValue()[that.index].cosa_migrare,
                     });
                     that.waitStart()
                     Server.route(r, function (json) {
@@ -123,34 +131,35 @@ export default {
                             return;
                         }
                         CrudCore.alertSuccess("Rollback eseguito con successo")
-                        that.view.reload();
+                        that.viewInstance.reload();
                     })
                 }
 
             },
 
-            'action-insert' : {
+            'action-insert': {
                 execute() {
                     let that = this;
                     let conf = cs.CrudVars.modelConfs.ModelCupparisEntity.popupInsert;
                     conf.actionsConfig['action-save'] = {
                         afterExecute() {
                             let thatA = this;
-                            console.debug('component json',this.json,thatA);
+                            console.debug('component json', this.json, thatA);
                             cs.CrudCore.waitStart();
-                            thatA.view.dialog.hide();
+                            thatA.viewInstance.dialogInstance.hide();
                             setTimeout(function () {
                                 document.location.href = '/#/manage/edit/ModelCupparisEntity/' + thatA.json.result.id;
                                 cs.CrudCore.waitEnd();
-                            },100)
+                            }, 100)
                         }
                     }
-                    cs.CrudCore.componentDialog('v-insert',conf,'Inserimento rapido campi');
+                    cs.CrudCore.componentDialog('v-insert', conf, 'Inserimento rapido campi');
                 }
             },
         },
         fields: [
             'id',
+            'cosa_migrare',
             'nome',
             'model_class',
 
@@ -158,6 +167,28 @@ export default {
         fieldsConfig: {
             id: {
                 type: 'w-hidden',
+            },
+            'cosa_migrare': {
+                label: 'Cosa migrare',
+                type: "w-checkbox",
+                // domainValues: {
+                //     'tutto': 'Tutto',
+                //     'migration': 'Migrazione',
+                //     'modello': 'Modello',
+                //     'policy': 'Policy',
+                //     'foorm': 'Foorm',
+                //     'modelconf': 'Modelconf',
+                //     'lang': 'Lang',
+                // },
+                // domainValuesOrder: [
+                //     'tutto',
+                //     'migration',
+                //     'modello',
+                //     'policy',
+                //     'foorm',
+                //     'modelconf',
+                //     'lang',
+                // ],
             },
             'nome': {
                 type: "w-text",
@@ -170,28 +201,38 @@ export default {
         orderFields: {
             'nome': 'nome',
 
+        },
+        afterDraw() {
+            // setTimeout(function () {
+            //
+            // console.log("QSA:::",document.querySelectorAll('.field-cosa_migrare'))
+            // document.querySelectorAll('td.field-cosa_migrare').forEach((item) => {
+            //     console.log("ITEM",item);
+            //     item.setAttribute('style','max-width:200px;');
+            // });
+            // },100)
         }
 
     },
 
-    popupInsert : {
-        type : 'v-insert',
-        modelName : 'cupparis_entity',
+    popupInsert: {
+        type: 'v-insert',
+        modelName: 'cupparis_entity',
         foormName: 'insert_express',
         actions: ['action-save'],
-        actionsConfig : {},
-        fields : [
+        actionsConfig: {},
+        fields: [
             'nome',
             'fields_text',
         ],
-        fieldsConfig : {
-            nome : {
+        fieldsConfig: {
+            nome: {
                 type: 'w-input',
-                rules : 'required',
+                rules: 'required',
             },
-            fields_text : {
-                type : 'w-textarea',
-                rules : 'required',
+            fields_text: {
+                type: 'w-textarea',
+                rules: 'required',
             },
         }
     },
@@ -222,16 +263,16 @@ export default {
             id: {
                 type: 'w-hidden',
             },
-            timestamps : {
+            timestamps: {
                 type: 'w-select',
             },
-            ownerships : {
+            ownerships: {
                 type: 'w-select',
             },
-            has_foto : {
-              type: 'w-radio',
+            has_foto: {
+                type: 'w-radio',
             },
-            has_attachments : {
+            has_attachments: {
                 type: 'w-radio',
             },
             'nome': {
@@ -239,7 +280,7 @@ export default {
                 change() {
                     var that = this;
 
-                    var mc = that.view.getWidget('model_class');
+                    var mc = that.viewInstance.getWidget('model_class');
                     // if (mc.getValue()) {
                     //     return;
                     // }
@@ -251,7 +292,7 @@ export default {
                 type: "w-input",
             },
             'columns_list': {
-              type: 'w-multi-select',
+                type: 'w-multi-select',
             },
             'columns_order': {
                 type: 'w-multi-select',
@@ -260,17 +301,17 @@ export default {
                 type: 'w-hasmany',
                 hasmanyType: 'list',
                 layout: {
-                  colClass: 'col-span-12',
+                    colClass: 'col-span-12',
                 },
                 hasmanyConf: {
-                    actions : [
-                      'action-delete',
+                    actions: [
+                        'action-delete',
                         'action-insert'
                     ],
-                    actionsConfig : {
-                      'action-delete' : {
-                          actionType: 'record',
-                      }
+                    actionsConfig: {
+                        'action-delete': {
+                            actionType: 'record',
+                        }
                     },
                     fields: [
                         'id',
@@ -296,34 +337,34 @@ export default {
                         id: {
                             type: 'w-hidden',
                         },
-                        tipo : {
+                        tipo: {
                             type: 'w-select',
                         },
-                        nullable : {
+                        nullable: {
                             type: 'w-select',
                         },
-                        index : {
+                        index: {
                             type: 'w-select',
                         },
-                        default : {
+                        default: {
                             type: 'w-input',
                         },
-                        on_delete : {
+                        on_delete: {
                             type: 'w-select',
                         },
-                        on_update : {
+                        on_update: {
                             type: 'w-select',
                         },
-                        'relazione_tabella' : {
+                        'relazione_tabella': {
                             type: 'w-select',
                         },
-                        'model_conf_search' : {
+                        'model_conf_search': {
                             type: 'w-select',
                         },
-                        'model_conf_list' : {
+                        'model_conf_list': {
                             type: 'w-select',
                         },
-                        'model_conf_edit' : {
+                        'model_conf_edit': {
                             type: 'w-select',
                         },
 
