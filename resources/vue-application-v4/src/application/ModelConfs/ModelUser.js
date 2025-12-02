710f3b6e5f9c3f99a96b07ecbdf9b608701454ae
Giacomo Terreni <giacomo.terreni@gmail.com>
2025-12-02 17:43:26 +0100

    modelconfs aggiornati

--- resources/vue-application-v4/src/application/ModelConfs/ModelUser.js
+++ resources/vue-application-v4/src/application/ModelConfs/ModelUser.js
@@ -1,35 +1,114 @@
+export default () => {
+    return {
+        "modelName": "user",
+        "search": {
+            modelName: 'user',
+            type: "v-search",
+            fields: [
+                "roles|id",
+            ],
+            fieldsConfig: {
+                'roles|id': {
+                    type: "w-select",
+                },
 
-export default {
-    "modelName" : "user",
-    "search": {
-        "modelName" : "user",
-        "type" : "v-search",
-        "fields": ["id","name","email","email_verified_at","banned","password","remember_token","created_at","updated_at","created_by","updated_by"],
-        "fieldsConfig": {},
-    },
-    "list": {
-        "modelName" : "user",
-        "type" : "v-list",
-        "actions" : [
-            "action-insert",
-            "action-edit",
-            "action-delete",
-            "action-delete-selected",
-        ],
-        "fields": ["id","name","email","email_verified_at","banned","password","remember_token","created_at","updated_at","created_by","updated_by"],
-        "fieldsConfig": {
-            remember_token : {
-                type :'w-text',
-                label : 'pupu',
-            }
+            },
+            advancedFields: [],
+            actionsConfig: {},
+            searchWithButton: true,
         },
-        "orderFields" : {},
-    },
-    "edit": {
-        "type" : "v-edit",
-        "modelName" : "user",
-        "actions" : ["action-save","action-back"],
-        "fields": ["id","name","email","email_verified_at","banned","password","remember_token","created_at","updated_at","created_by","updated_by"],
-        "fieldsConfig":{},
-    },
+        "list": {
+            "modelName": "user",
+            "type": "v-list",
+            "actions": [
+                "action-insert",
+                "action-edit",
+                "action-delete",
+                "action-delete-selected",
+            ],
+            "fields": [
+                "id",
+                "name",
+                "email",
+                "email_verified_at",
+                "banned",
+                "mainrole"
+            ],
+            "fieldsConfig": {
+                email_verified_at: {
+                    type: 'w-swap',
+                    modelName: 'user',
+                },
+                banned: {
+                    type: 'w-swap',
+                    modelName: 'user',
+                    switchClass: 'form-switch-danger banned',
+                    dataSwitched: true,
+                    label: 'Bloccato',
+                },
+            },
+            "orderFields": {},
+        },
+        edit: {
+            modelName: 'user',
+            type: 'v-edit',
+            actions: ['action-save', 'action-save-back', 'action-back'],
+            fields: [//'info',
+                'email',
+                'name',
+                'password', 'password_confirmation',
+                'mainrole',
+                // 'empty',
+                // 'fotos','attachments'
+            ],
+            afterDraw() {
+
+            },
+
+            methods: {
+                fillData: function (route, json) {
+                    var that = this;
+                    if (json.metadata.is_auth) {
+                        that.fieldsConfig.mainrole = 'w-hidden';
+                    }
+                    that.$options.methods.fillData.apply(that, [route, json]);
+                },
+                completed() {
+                    // console.log('widget mainrole',this.getWidget('mainrole'))
+                    // var widget = this.getWidget('mainrole');
+                    // if (widget.type === 'w-hidden') {
+                    //     this.getWidget('asl_id').jQe().closest('.asl_id').addClass('d-none');
+                    // } else
+                    //     widget.showHideUsl();
+                }
+            },
+            fieldsConfig: {
+                mainrole: {
+                    type: 'w-select',
+                    change() {
+                        console.log('mainrole value', this.getValue())
+                        var value = this.getValue();
+                        if (value === 5) {
+                            this.view.showWidget('scuola_id');
+                        } else {
+                            this.view.hideWidget('scuola_id');
+                        }
+                    }
+
+                },
+                banned: 'w-radio',
+                password: {
+                    type: 'w-input',
+                    inputType: 'password',
+                },
+                password_confirmation: {
+                    type: 'w-input',
+                    inputType: 'password',
+                },
+                //roles : 'w-select',
+            },
+            actionsConfig: {}
+        },
+
+    }
 }
